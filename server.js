800e74225b67f373079adbdf7725f9cf5f762014
temiloluwa2000 <101112302+temiloluwa2000@users.noreply.github.com>
2022-04-05 14:55:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ myapp.get('/', function(req, res) {
    res.sendFile( __dirname);
    res.sendFile(path.join(__dirname + '/sport/index.html'));
 });
+myapp.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, myapp.settings.env);
+});
