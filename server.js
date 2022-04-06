72f2640f3058749726c8565f7524c7b57923a1f4
temiloluwa2000 <101112302+temiloluwa2000@users.noreply.github.com>
2022-04-06 14:59:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ myapp.get('/', function(req, res) {
    res.sendFile( __dirname);
    res.sendFile(path.join(__dirname + '/sport/indexx.html'));
 });
-myapp.use(express.static(__dirname + '/sport'));
+myapp.use(express.static(__dirname + 'sport'));
 myapp.use(bodyParser.urlencoded({ extended: true }));
 myapp.use(bodyParser.json());
 
